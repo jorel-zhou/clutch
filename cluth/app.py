33fa4bdb856ec7d103355350056b92d6a91dd118
Jun Zhou <jorel_zhou@outlook.com>
2016-09-06 22:53:14 +0800

    initialzed project structure

---
+++ cluth/app.py
@@ -0,0 +1,17 @@
+'''
+Created on Sep 6, 2016
+
+@author: junzhou
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
